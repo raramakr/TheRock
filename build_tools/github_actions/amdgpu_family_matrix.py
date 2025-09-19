1d8a2434937966e1130a08bb6d8b350d155c3a68
geomin12 <geomin12@amd.com>
2025-09-19 07:22:17 -0700

    Adding linux gfx1151

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -28,9 +28,10 @@ amdgpu_family_info_matrix_presubmit = {
     },
     "gfx115x": {
         "linux": {
-            "test-runs-on": "",
+            "test-runs-on": "linux-strix-halo-gpu-rocm",
             "family": "gfx1151",
             "bypass_tests_for_releases": True,
+            "sanity_check_only": True
         },
         "windows": {
             "test-runs-on": "windows-strix-halo-gpu-rocm",
