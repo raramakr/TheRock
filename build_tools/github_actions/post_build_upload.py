4025bed07d2234052fe5a6ae8b1df1577f635896
geomin12 <geomin12@amd.com>
2025-09-23 16:33:12 -0700

    Adding time sync and region for s3 upload

--- build_tools/github_actions/post_build_upload.py
+++ build_tools/github_actions/post_build_upload.py
@@ -151,6 +151,11 @@ def upload_artifacts(args: argparse.Namespace, bucket_uri: str):
     build_dir = args.build_dir
     amdgpu_family = args.amdgpu_family
 
+    # AWS signatures depend on timestamps and the Windows machine clock may differ by 5mins
+    # This will make sure the Windows time is synced
+    if is_windows:
+        exec(["w32tm", "/resync"], cwd=Path.cwd())
+
     # Uploading artifacts to S3 bucket
     cmd = [
         "aws",
@@ -164,6 +169,8 @@ def upload_artifacts(args: argparse.Namespace, bucket_uri: str):
         "*",
         "--include",
         "*.tar.xz*",
+        "--region",
+        "us-east-2",
     ]
     exec(cmd, cwd=Path.cwd())
 
