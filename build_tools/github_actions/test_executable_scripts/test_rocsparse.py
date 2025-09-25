a652c249b760551bbb47f503f81d32769bda9dd3
Joseph Macaranas <Joseph.Macaranas@amd.com>
2025-09-25 15:15:59 -0400

    BLAS test coverage adjustments
    - Adjusting test coverage for BLAS to match what I observe in Math CI logs.
    - `hipblaslt` does not actually pass any gtest filters in their pre-checkin workflow.
    - The `rocblas_smoke.yaml` only checks if the API exists, so it is replaced to match the Math CI pre-checking workflow.
    - Observed spelling errors and corrected across these test scripts.

--- build_tools/github_actions/test_executable_scripts/test_rocsparse.py
+++ build_tools/github_actions/test_executable_scripts/test_rocsparse.py
@@ -14,10 +14,10 @@ logging.basicConfig(level=logging.INFO)
 # GTest sharding
 SHARD_INDEX = os.getenv("SHARD_INDEX", 1)
 TOTAL_SHARDS = os.getenv("TOTAL_SHARDS", 1)
-envion_vars = os.environ.copy()
+environ_vars = os.environ.copy()
 # For display purposes in the GitHub Action UI, the shard array is 1th indexed. However for shard indexes, we convert it to 0th index.
-envion_vars["GTEST_SHARD_INDEX"] = str(int(SHARD_INDEX) - 1)
-envion_vars["GTEST_TOTAL_SHARDS"] = str(TOTAL_SHARDS)
+environ_vars["GTEST_SHARD_INDEX"] = str(int(SHARD_INDEX) - 1)
+environ_vars["GTEST_TOTAL_SHARDS"] = str(TOTAL_SHARDS)
 
 cmd = [
     f"{THEROCK_BIN_DIR}/rocsparse-test",
@@ -26,4 +26,4 @@ cmd = [
     f"{OUTPUT_ARTIFACTS_DIR}/clients/matrices/",
 ]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
-subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=envion_vars)
+subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=environ_vars)
