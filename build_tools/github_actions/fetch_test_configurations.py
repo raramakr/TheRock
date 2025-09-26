a0e3604af71f823bb2bc5b5a68c6df33f61d30d9
Joseph Macaranas <Joseph.Macaranas@amd.com>
2025-09-25 22:47:45 -0400

    Upping rocblas timeout
    - From vague knowledge of other sharding work on rocblas

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -32,10 +32,10 @@ test_matrix = {
     "rocblas": {
         "job_name": "rocblas",
         "fetch_artifact_args": "--blas --tests",
-        "timeout_minutes": 15,
+        "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_rocblas.py')}",
         "platform": ["linux", "windows"],
-        "total_shards": 1,
+        "total_shards": 4,
     },
     "hipblaslt": {
         "job_name": "hipblaslt",
