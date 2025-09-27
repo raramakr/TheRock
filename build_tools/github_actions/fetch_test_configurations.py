e04d5c548b852a885d70e3c6b59dc7be51a03ac5
Joseph Macaranas <Joseph.Macaranas@amd.com>
2025-09-26 23:56:29 -0400

    Populate exclude list for rocblas
    - Also increase timeout for hipblas

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -32,18 +32,18 @@ test_matrix = {
     "rocblas": {
         "job_name": "rocblas",
         "fetch_artifact_args": "--blas --tests",
-        "timeout_minutes": 120,
+        "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_rocblas.py')}",
         "platform": ["linux", "windows"],
-        "total_shards": 1,
+        "total_shards": 4,
     },
     "hipblas": {
         "job_name": "hipblas",
         "fetch_artifact_args": "--blas --tests",
-        "timeout_minutes": 15,
+        "timeout_minutes": 30,
         "test_script": f"python {_get_script_path('test_hipblas.py')}",
         "platform": ["linux", "windows"],
-        "total_shards": 1,
+        "total_shards": 2,
     },
     "hipblaslt": {
         "job_name": "hipblaslt",
--- build_tools/github_actions/test_executable_scripts/test_rocblas.py
+++ build_tools/github_actions/test_executable_scripts/test_rocblas.py
@@ -8,6 +8,9 @@ THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 
+PLATFORM = os.getenv("PLATFORM")
+AMDGPU_FAMILIES = os.getenv("AMDGPU_FAMILIES")
+
 # GTest sharding
 SHARD_INDEX = os.getenv("SHARD_INDEX", 1)
 TOTAL_SHARDS = os.getenv("TOTAL_SHARDS", 1)
@@ -22,5 +25,30 @@ cmd = [
     f"{THEROCK_BIN_DIR}/rocblas-test",
     f"--gtest_filter=*quick*:*pre_checkin*-*known_bug*",
 ]
+
+tests_to_exclude = {
+    # Related issue: https://github.com/ROCm/TheRock/issues/1605
+    # observed seg faults all have dot functions, f32_c type, batch count 257, among other similarities
+    "gfx942": {
+        "linux":
+            [
+                "_/dot_batched.blas1/quick_blas1_batched_f32_c_13000_n3_n3_257_0",
+                "_/dotc_batched.blas1/quick_blas1_batched_f32_c_13000_n3_n3_257_0",
+                "_/dot_strided_batched.blas1/quick_blas1_strided_batched_f32_c_13000_n3_39000_n3_39000_257_0",
+                "_/dotc_strided_batched.blas1/quick_blas1_strided_batched_f32_c_13000_n3_39000_n3_39000_257_0",
+                "_/dot_batched_ex.blas1_ex/quick_blas1_batched_f32_c_f32_c_f32_c_f32_c_13000_n3_n3_257",
+                "_/dot_strided_batched_ex.blas1_ex/quick_blas1_strided_batched_f32_c_f32_c_f32_c_f32_c_13000_n3_39000_n3_39000_257",
+                "_/dotc_batched_ex.blas1_ex/quick_blas1_batched_f32_c_f32_c_f32_c_f32_c_13000_n3_n3_257",
+                "_/dotc_strided_batched_ex.blas1_ex/quick_blas1_strided_batched_f32_c_f32_c_f32_c_f32_c_13000_n3_39000_n3_39000_257",
+            ]
+    }
+}
+
+if AMDGPU_FAMILIES in tests_to_exclude and PLATFORM in tests_to_exclude.get(
+    AMDGPU_FAMILIES, {}
+):
+    exclusion_list = ":".join(tests_to_exclude[AMDGPU_FAMILIES][PLATFORM])
+    cmd.append(f"--gtest_filter=-{exclusion_list}")
+
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
 subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=environ_vars)
