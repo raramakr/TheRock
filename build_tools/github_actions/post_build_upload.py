f58b5b429ae580445a1749823e2ca737007a2d24
Justin Chen <justchen@amd.com>
2025-09-30 09:36:22 -0700

    Add missing commad to separate elements

--- build_tools/github_actions/post_build_upload.py
+++ build_tools/github_actions/post_build_upload.py
@@ -69,20 +69,20 @@ def sync_windows_clock():
 
     exec_pwsh(
         [
-            "Set-ItemProperty -Path"
-            '"HKLM:\\SYSTEM\\CurrentControlSet\\services\\W32Time\\Config"'
-            '-Name "AnnounceFlags" -Value 5'
-        ], 
-        cwd=Path.cwd()
+            "Set-ItemProperty -Path",
+            '"HKLM:\\SYSTEM\\CurrentControlSet\\services\\W32Time\\Config"',
+            '-Name "AnnounceFlags" -Value 5',
+        ],
+        cwd=Path.cwd(),
     )
 
     exec_pwsh(
         [
-            "Set-ItemProperty -Path"
-            '"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\w32time\\TimeProviders\\NtpServer"'
-            '-Name "Enabled" -Value 1'
-        ], 
-        cwd=Path.cwd()
+            "Set-ItemProperty -Path",
+            '"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\w32time\\TimeProviders\\NtpServer"',
+            '-Name "Enabled" -Value 1',
+        ],
+        cwd=Path.cwd(),
     )
 
     exec_pwsh(["Restart-Service w32Time"])
