01498a1213a14645ca59dc13945410ad1bfef62b
Justin Chen <justchen@amd.com>
2025-09-30 08:12:51 -0700

    Add back current working directory for powershell

--- build_tools/github_actions/post_build_upload.py
+++ build_tools/github_actions/post_build_upload.py
@@ -72,7 +72,8 @@ def sync_windows_clock():
             "Set-ItemProperty -Path"
             '"HKLM:\\SYSTEM\\CurrentControlSet\\services\\W32Time\\Config"'
             '-Name "AnnounceFlags" -Value 5'
-        ]
+        ], 
+        cwd=Path.cwd()
     )
 
     exec_pwsh(
@@ -80,7 +81,8 @@ def sync_windows_clock():
             "Set-ItemProperty -Path"
             '"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\w32time\\TimeProviders\\NtpServer"'
             '-Name "Enabled" -Value 1'
-        ]
+        ], 
+        cwd=Path.cwd()
     )
 
     exec_pwsh(["Restart-Service w32Time"])
